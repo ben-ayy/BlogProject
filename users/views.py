e5ef18a039ccdcc1df26f371b79fdae25f86220f
ben-ayy <c2003386@ncl.ac.uk>
2023-10-03 15:09:13 +0100

    Added login form with GET and POST requests

--- users/views.py
+++ users/views.py
@@ -1,11 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from users.forms import RegisterForm
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
 
-@users_blueprint.route('/register')
+@users_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('users/register.html')
+    registerForm = RegisterForm()
+
+    if registerForm.validate_on_submit():
+        print(request.form.get('username'))
+        print(request.form.get('password'))
+        return redirect(url_for('users.login'))
+
+    return render_template('users/register.html', form=registerForm)
 
 
 @users_blueprint.route('/login')
